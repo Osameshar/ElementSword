4e62ec463f2e859a2caea6b55d20e1535626d8fc
cakeller43 <cakeller43@gmail.com>
2015-02-16 21:51:16 -0600

    Dward Run Animation

--- 2d Action Adventure/Assets/Scripts/Enemy/CombatManagerEnemy.cs
+++ 2d Action Adventure/Assets/Scripts/Enemy/CombatManagerEnemy.cs
@@ -5,6 +5,8 @@ public class CombatManagerEnemy : MonoBehaviour
 {
 	Stats stats;
 	BuffDebuffManager bdManager;
+	private bool invulnerable = false;
+	public float timeBetweenDamage = 1f;
 
 	void Start () 
 	{
@@ -22,9 +24,20 @@ public class CombatManagerEnemy : MonoBehaviour
 	}
 	public void TakeDamage(float damage, BuffDebuff bd)
 	{
+		if (invulnerable)
+			return;
 		damage = damage/stats.getToughness();
 		stats.alterHealth(damage);//multiply by toughness or whatever formula we need to use
 		if(bd != null)
 			bdManager.AddBuffDebuff (bd);
+	
+		invulnerable = true;
+		StartCoroutine (InvulnerableTime ());
+	}
+
+	IEnumerator InvulnerableTime()
+	{
+		yield return new WaitForSeconds(timeBetweenDamage);
+		invulnerable = false;
 	}
 }
---
+++ 2d Action Adventure/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour 
+{
+	
+	public float distance;//distance to patrol
+	
+	private Animator anim;
+	private Stats stats;
+	
+	private float speed;
+	private int action = 0;
+	Vector3 originalPosition;
+	bool isGoingLeft = false;
+	
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		stats = GetComponent<Stats> ();
+		originalPosition = transform.position;
+		action = 1;
+	}
+	
+	void Update () 
+	{
+		speed = stats.getSpeed() * stats.getMovementModifier();
+		anim.SetFloat ("Speed", speed);
+		
+		switch (action) 
+		{
+		case 1:
+		{
+			isPatrolling();
+			break;
+		}
+		case 2:
+		{
+			isChasing();
+			break;
+		}
+		case 3:
+		{
+			isReturning();
+			break;
+		}
+		default:
+		{
+			break;
+		}
+		}
+	}
+	
+	void isPatrolling()
+	{
+		float distFromStart = transform.position.x - originalPosition.x;   
+		
+		if (isGoingLeft)
+		{
+			// If gone too far, switch direction
+			if (distFromStart < -distance)
+				SwitchDirection();
+			transform.Translate (new Vector3(Time.deltaTime * speed * -1,0,0));
+		}
+		else
+		{
+			// If gone too far, switch direction
+			if (distFromStart > distance)
+				SwitchDirection();
+			
+			transform.Translate (new Vector3(Time.deltaTime * speed,0,0));
+		}
+	}
+	
+	void isChasing()
+	{
+		
+	}
+	void isReturning()
+	{
+		
+	}
+	
+	void SwitchDirection()
+	{
+		isGoingLeft = !isGoingLeft;
+		Flip ();
+		//TODO: Change facing direction, animation, etc
+	}
+	
+	void Flip()
+	{
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	
+}
